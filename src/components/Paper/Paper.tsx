8c8ac049b6ac8517136031134d851cf59ba88a8c
Matthew Pagan <mastapegs01@gmail.com>
2021-09-07 11:42:09 -0400

    fix styles for paper component

--- src/components/Paper/Paper.tsx
+++ src/components/Paper/Paper.tsx
@@ -1,7 +1,10 @@
 import { FC } from "react";
 
 const Paper: FC = ({ children }) => (
-  <div id="paper" className="bg-white p-4 border rounded shadow-lg">
+  <div
+    id="paper"
+    className="bg-white p-4 border rounded shadow-lg max-w-screen-sm mx-auto"
+  >
     {children}
   </div>
 );
