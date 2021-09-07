1455f06343849a074b7ca9d25001102cfe1d7b54
Matthew Pagan <mastapegs01@gmail.com>
2021-09-07 08:15:40 -0400

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { FC } from "react";
+
+const App: FC = () => {
+  return (
+    <div className="bg-gray-50 p-4 min-h-screen">
+      <h1>CRA Starter</h1>
+    </div>
+  );
+};
+
+export default App;
