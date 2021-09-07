1c7703d94a07a3bc8c4b20691056c3c3a6ed3c94
Matthew Pagan <mastapegs01@gmail.com>
2021-09-07 08:25:41 -0400

    feat: improve base styles

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,14 @@ import { FC } from "react";
 
 const App: FC = () => {
   return (
-    <div className="bg-gray-50 p-4 min-h-screen">
-      <h1>CRA Starter</h1>
+    <div id="background" className="bg-gray-50 p-4 min-h-screen">
+      <div id="paper" className="bg-white p-4 border rounded shadow-lg">
+        <h1 className="text-3xl font-semibold mb-4">Custom React Renderer</h1>
+        <p>
+          We are going to render to the DOM using our own implementation of
+          ReactDOM.
+        </p>
+      </div>
     </div>
   );
 };
