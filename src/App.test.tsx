14e1de0c32831d7eccb8b38e1c5d0267e0d4be0c
Matthew Pagan <mastapegs01@gmail.com>
2021-09-07 08:28:35 -0400

    edit tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,6 +3,6 @@ import App from "./App";
 
 test("renders learn react link", () => {
   render(<App />);
-  const linkElement = screen.getByText(/CRA Starter/i);
+  const linkElement = screen.getByText(/Custom React Renderer/i);
   expect(linkElement).toBeInTheDocument();
 });
